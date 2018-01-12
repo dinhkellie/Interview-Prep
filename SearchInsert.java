05a627fbfefbe3946ca13d50cd70a3b0a5b66a4a
Kellie Dinh <dinhkellie@yahoo.com>
2018-01-12 00:38:47 -0500

    Update SearchInsert.java

--- SearchInsert.java
+++ SearchInsert.java
@@ -4,19 +4,19 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int low = 0;
         int mid = 0;
-        int high = nums.length; // high = 4
+        int high = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            mid = low + ((high-low)/2); // mid = 2, 3, 3
+            mid = low + ((high-low)/2);
             
             if (mid > nums.length-1 && nums.length != 1) {
                 return nums.length;
             }
-            if (target == nums[mid]) { // 7 != 5, 7 != 6
+            if (target == nums[mid]) {
                 return mid;
             } else if (target < nums[mid]) { 
                 high = mid; 
             } else { // 7 > 5, 7 > 6
-                low = mid; // low = 2, 3 
+                low = mid;
             }
         }
         if (target > nums[low]) {
